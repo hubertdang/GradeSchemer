99ccb336cd16bb67bb2f544695b2fd627431350f
hubertdang <hubertdang@gmail.com>
2022-12-29 01:27:37 -0500

    Changed the program to perform most user input before function calls to make functions easier to test

--- GradeSchemer.py
+++ GradeSchemer.py
@@ -29,10 +29,48 @@ while quit == False:
                         commands_GradeSchemer.current_average(courses_dict, course)
                     elif usr_cmd == 'B':
                         course = input("\nEnter the name of the course of interest: ")
-                        commands_GradeSchemer.final_exam_goal(courses_dict, course)
-                        
+                        if course not in courses_dict.keys():
+                            print("Invalid course\n")
+                            # Break out of command loop
+                            break
+                        # Checking if the course is valid for this command
+                        elif commands_GradeSchemer.add_weightings(courses_dict, course) == 100:
+                            print("You have already completed the final exam in this course")
+                            # Break out of command loop
+                            break
 
-            
+                        temp_dict = courses_dict
+                        goal = float(input("Enter your overall grade goal as digits: "))
+                        exam_weight = float(input("Enter the weighting of your final exam as digits: "))
+
+                        weighting = commands_GradeSchemer.add_weightings(courses_dict, course)
+
+                        if ((goal < commands_GradeSchemer.add_grade_points(temp_dict, course)) or ((exam_weight + weighting) > 100)):
+                            while (goal < commands_GradeSchemer.add_grade_points(temp_dict, course)):
+                                goal = float(input("The goal entered was invalid. Enter your overall grade goal as digits again: "))
+                            while (exam_weight + weighting) > 100:
+                                exam_weight = float(input("The final exam weighting entered was too high. Enter the weighting of your final exam as digits again: "))
+                        
+                        total_weight = weighting + exam_weight
+                        # Fill in empty data on assessments
+                        while total_weight < 100:
+                            print("\nThere are some missing assessments.\n")
+                            print("Your total weighting is currently " + str(total_weight) + "/100, the remaining weight is " + str(100 - total_weight) + "\n")
+                            name = input("Enter an ungraded/incomplete assessment name: ")
+                            weight = float(input("Enter the assessment's weighting: "))
+                            grade = float(input("Enter a estimated grade for the assessment: "))
+                            temp_dict[course] += [[name, weight, grade]]
+                            total_weight += weight
+
+                            if total_weight > 100:
+                                total_weight -= weight
+                                print("\nThe weighting of that assessment is invalid (too high)\n")
+                        commands_GradeSchemer.final_exam_goal(courses_dict, course, goal, exam_weight, temp_dict)
+
+                            
+                            
+
+                
 
 
 
